5f0d7b03fec664fc54ed95ef65dba7394c868df8
Micha Kops <micha.kops@gmail.com>
2014-12-27 16:32:14 +0100

    Initial import.

---
+++ src/test/java/feature/BookSearchTest.java
@@ -0,0 +1,12 @@
+package feature;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+// @CucumberOptions(features = {
+// "src/test/resources/feature/search_book.feature" }, glue = "feature")
+public class BookSearchTest {
+
+}
