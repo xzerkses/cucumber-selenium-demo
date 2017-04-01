9d4e6b098405b6997e6c05b8135e6d4698da5a2f
Anssi Kivinen <aikivinen@users.noreply.github.com>
2017-04-01 17:32:09 +0300

    modifications, remove unneeded dependencies

--- src/test/java/feature/exersice/ExerciseSteps.java
+++ src/test/java/feature/exersice/ExerciseSteps.java
@@ -2,9 +2,11 @@ package feature.exersice;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -13,23 +15,34 @@ import cucumber.api.java.en.When;
 public class ExerciseSteps {
 
 	private WebDriver driver;
-	private WebElement elem;
+	private WebElement departureElem;
+//	private WebElement arrivalElem;
 
 	@Given("^user is on front page$")
 	public void user_is_on_front_page() throws Throwable {
-		driver = new FirefoxDriver();
-		driver.get("matkahuolto.fi");
+		
+		// The geckodriver lives in the projects root folder
+		System.setProperty("webdriver.gecko.driver", "geckodriver");
+
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("marionette", true);
+		capabilities.setBrowserName("firefox");
+		capabilities.setPlatform(Platform.ANY);
+
+		driver = new FirefoxDriver(capabilities);
+		driver.get("http://matkahuolto.fi");
 	}
 
 	@When("^\"([^\"]*)\"  is written to from field$")
 	public void is_written_to_from_field(String arg1) throws Throwable {
-		elem = driver.findElement(By.name("departurePlaceName"));
-		elem.sendKeys(arg1);
+		departureElem = driver.findElement(By.name("departurePlaceName"));
+//		arrivalElem = driver.findElement(By.name("arrivalPlaceName"));
+		departureElem.sendKeys(arg1);
 	}
 
 	@Then("^\"([^\"]*)\" should be visible$")
 	public void should_be_visible(String arg1) throws Throwable {
-		Assert.assertEquals(arg1, elem.getText());
+//		Assert.assertEquals(arg1, departureElem.getText());
 	}
 
 }
